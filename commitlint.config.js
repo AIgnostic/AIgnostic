f29ba1d40fd3fe351e44f84659b8348b619e3e65
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2025-01-15 11:29:38 +0000

    (ci): Add poetry install [kss22]

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,18 +1,27 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'type-enum': [
-        2,
-        'always',
-        ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'setup'],
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "chore",
+        "setup",
+        "ci",
       ],
-      'subject-case': [2, 'always', 'sentence-case'],
+    ],
+    "subject-case": [2, "always", "sentence-case"],
+  },
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^\((?<type>[^)]+)\):\s(?<subject>.+)$/,
+      headerCorrespondence: ["type", "subject"],
     },
-    parserPreset: {
-      parserOpts: {
-        headerPattern: /^\((?<type>[^)]+)\):\s(?<subject>.+)$/,
-        headerCorrespondence: ['type', 'subject'],
-      },
-    },
-  };
-  
+  },
+};
