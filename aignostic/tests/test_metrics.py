e44936d76812facb72d8aab118a3ca0e4bb649c3
Tisya Varghese [tsv22] <tsv22@ic.ac.uk>
2025-01-17 15:52:24 +0000

    (Feat): Basic accuracy/precision/recall [tsv22][sk4122]

---
+++ aignostic/tests/test_metrics.py
@@ -0,0 +1,28 @@
+import numpy as np
+from aignostic.metrics.metrics import calculate_metrics
+"""
+    Test calculate_metrics calls all metrics
+"""
+
+def test_calculate_metrics():
+    # Use 8 data points for testing
+    y_true = np.array([1, 0, 1, 1, 0, 1, 0, 0])
+    y_pred = np.array([1, 0, 1, 0, 0, 1, 1, 0])
+    metrics = ["accuracy", "precision", "recall"]
+
+    results = calculate_metrics(y_true, y_pred, metrics)
+    assert "accuracy" in results and results["accuracy"] == 0.75
+    assert "precision" in results and results["precision"] == 0.75
+    assert "recall" in results and results["recall"] == 0.75
+
+
+def test_only_calcs_specified_metrics():
+    # Use 8 data points for testing
+    y_true = np.array([1, 0, 1, 1, 0, 1, 0, 0])
+    y_pred = np.array([1, 0, 1, 0, 0, 1, 1, 0])
+    metrics = ["accuracy", "recall"]
+
+    results = calculate_metrics(y_true, y_pred, metrics)
+    assert "accuracy" in results
+    assert "precision" not in results
+    assert "recall" in results
