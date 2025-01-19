79501a5a11d5bd8431cb1932b2df678b3e83a6f1
Tisya Varghese [tsv22] <tsv22@ic.ac.uk>
2025-01-19 20:15:07 +0000

    (Feat): Update UI with Stepper, adding selection of metrics [tsv22]

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -9,76 +9,7 @@ export function App() {
     <div>
       {/* <NxWelcome title="AIgnostic" /> */}
       <Homepage />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={<Home />}
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
     </div>
   );
 }
-
-function Home() {
-  const [text1, setText1] = useState('');
-  const [text2, setText2] = useState('');
-
-  const handleSubmit = () => {
-    if (text1 && text2) {
-      console.log('Textbox 1:', text1);
-      console.log('Textbox 2:', text2);
-    } else {
-      alert('Please fill in both textboxes before submitting.');
-    }
-  };
-
-  return (
-    <div>
-      <h1>Enter Text</h1>
-      <input
-        type="text"
-        placeholder="Enter text 1"
-        value={text1}
-        onChange={(e) => setText1(e.target.value)}
-      />
-      <br />
-      <input
-        type="text"
-        placeholder="Enter text 2"
-        value={text2}
-        onChange={(e) => setText2(e.target.value)}
-      />
-      <br />
-      <button onClick={handleSubmit}>Submit</button>
-    </div>
-  );
-}
-
-
 export default App;
--- frontend/src/app/home.tsx
+++ frontend/src/app/home.tsx
@@ -1,52 +1,185 @@
 import React, { useState } from 'react';
 import checkURL from './utils';
+import { Box, Button, Chip, TextField } from '@mui/material';
+import { Stepper, Step, StepLabel, StepContent, Typography } from '@mui/material';
+
+const steps = [
+  {
+    label: 'Enter model and dataset API URLs',
+    description: `Enter the URLs for your model and dataset APIs to get started.
+                  For more information about creating the APIs, see the documentation - click 'Getting Started'.`,
+  },
+  {
+    label: 'Select Legislation',
+    description:
+      `Select the legislations that you want to comply with.`,
+  },
+  {
+    label: 'Select Metrics',
+    description:
+      `Select the metrics you want to analyze your model with. 
+       These will be used to quantify your compliance score with your selected metrics.`,
+  },
+  {
+    label: 'Summary and Generate Report',
+    description: `Check that you are happy with your selections and generate your compliance report.
+                  Report generation may take some time.`,
+  },
+];
+
+const metrics = ["Accuracy", "Precision", "Recall"];
+
+
 function Homepage() {
   const [modelURL, setModelURL] = useState('');
   const [datasetURL, setDatasetURL] = useState('');
+  const [isModelURLValid, setIsModelURLValid] = useState(true);
+  const [isDatasetURLValid, setIsDatasetURLValid] = useState(true);
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [metricChips, setMetricChips] = useState(metrics.map((metric) => {return {"label": metric, "selected": true}}));
+
 
   const handleSubmit = () => {
     if (modelURL && datasetURL) {
-        //Enter into SanityCheckService
-      // alert(`Input 1: ${modelURL}, Input 2: ${datasetURL}`);
-      // alert(checkURL(modelURL) && checkURL(datasetURL))
       console.log(`Input 1: ${modelURL}, Input 2: ${datasetURL}`);
+      console.log(`Selected Metrics: ${metricChips.filter((metricChip) => metricChip.selected).map((metricChip) => metricChip.label).join(", ")}`);
       console.log(checkURL(modelURL) && checkURL(datasetURL))
+      
+      const user_info = {"modelURL": modelURL, 
+        "datasetURL": datasetURL,
+        "metrics": metricChips.filter((metricChip) => metricChip.selected).map((metricChip) => metricChip.label)
+      }
+      
+      // send user_info to controller 
     } else {
       console.log('Please fill in both text inputs.');
       // alert('Please fill in both text inputs.');
     }
   };
 
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+
+  
+  
+
   return (
-    <div style={styles.container}>
-      {/* Logo or Welcome Message */}
-      <div style={styles.logoContainer}>
-        <h1 style={styles.logoText}>Welcome to AIgnostic</h1>
-      </div>
+    <Box sx={[styles.container ]}>
+      <Box style={styles.container}>
+        <Box style={styles.logoContainer}>
+          
+          <h1 style={styles.logoText}>AIgnostic</h1>
+        </Box>
 
-      {/* Text Inputs */}
-      <div style={styles.formContainer}>
-        <input
-          type="text"
-          placeholder="Enter Model API URL"
-          value={modelURL}
-          onChange={(e) => setModelURL(e.target.value)}
-          style={styles.input}
-        />
-        <input
-          type="text"
-          placeholder="Enter Dataset API URL"
-          value={datasetURL}
-          onChange={(e) => setDatasetURL(e.target.value)}
-          style={styles.input}
-        />
+        <Box style={styles.horizontalContainer}>
+          New to AIgnostic? Read the docs to get started: 
+          <Button onClick={()=>{}} style={styles.button}>
+            Getting Started
+          </Button>
+        </Box> 
+      </Box>
+      
+      <Stepper activeStep={activeStep} style={{width: "80%"}} orientation="vertical">
+        {steps.map((step, index) => (
+          <Step key={step.label}>
+            <StepLabel
+              optional={
+                index === steps.length - 1 ? (
+                  <Typography variant="caption">Last step</Typography>
+                ) : null
+              }
+            >
+              {step.label}
+            </StepLabel>
+            <StepContent>
+              <Typography>{step.description}</Typography>
 
-        {/* Submit Button */}
-        <button onClick={handleSubmit} style={styles.button}>
-          Submit
-        </button>
-      </div>
-    </div>
+              {index === 0 && 
+                  (<Box>
+                      <TextField
+                        type="text"
+                        placeholder="Enter Model API URL"
+                        value={modelURL}
+                        onChange={(e) => setModelURL(e.target.value)}
+                        onBlur={() => setIsModelURLValid(checkURL(modelURL))}
+                        style={styles.input}
+                        error={!!modelURL && !isModelURLValid}
+                        helperText={(!!modelURL && !isModelURLValid) ? 'Invalid URL' : ''}
+                      />
+                      <TextField
+                        type="text"
+                        placeholder="Enter Dataset API URL"
+                        value={datasetURL}
+                        onChange={(e) => setDatasetURL(e.target.value)}
+                        onBlur={() => setIsDatasetURLValid(checkURL(datasetURL))}
+                        style={styles.input}
+                        error={!!datasetURL && !isDatasetURLValid}
+                        helperText={(!!datasetURL && !isDatasetURLValid) ? 'Invalid URL' : ''}
+                      />
+                      
+                    </Box>)
+              }
+
+              {index === 2 && 
+                  (<Box>
+                    {metricChips.map((metricChip) => (
+                      <Chip 
+                        label={metricChip.label}
+                        variant="filled"
+                        onDelete={() => {metricChip.selected = !metricChip.selected; setMetricChips([...metricChips])}}
+                        onClick={() => {metricChip.selected = !metricChip.selected; setMetricChips([...metricChips])}}
+                        color={metricChip.selected ? "primary" : "default"}
+                        style={{margin: '5px'}} 
+                      />
+                    ))}
+                  </Box>)
+              }
+
+              {index === steps.length - 1 &&
+                (<Box>
+                  <h3>Summary</h3>
+                  <p>
+                    Model URL: {modelURL}
+                    <br/> <br/>
+                    Dataset URL: {datasetURL} 
+                    <br/> <br/>
+                    Metrics: {metricChips.filter((metricChip) => metricChip.selected).map((metricChip) => metricChip.label).join(", ")}
+                  </p>
+                </Box>)
+                  }
+
+
+              <Box sx={{ mb: 2 }}>
+                <Button
+                  variant="contained"
+                  onClick={handleNext}
+                  sx={{ mt: 1, mr: 1 }}
+                >
+                  {index === steps.length - 1 ? 'Generate Report' : 'Next'}
+                </Button>
+                <Button
+                  disabled={index === 0}
+                  onClick={handleBack}
+                  sx={{ mt: 1, mr: 1 }}
+                >
+                  Back
+                </Button>
+              </Box>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+  </Box>
   );
 }
 
@@ -56,31 +189,33 @@ const styles : { [key: string]: React.CSSProperties} = {
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    height: '100vh',
+    height: '100%',
     fontFamily: 'Arial, sans-serif',
     backgroundColor: '#f5f5f5',
+    width: '100%',
+    paddingTop: '20px',
   },
-  logoContainer: {
-    marginBottom: '20px',
+  horizontalContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   logoText: {
     fontSize: '36px',
     fontWeight: 'bold',
     color: '#333',
+    fontFamily: 'serif',
   },
   formContainer: {
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
-    width: '300px',
   },
   input: {
     width: '100%',
     padding: '10px',
     marginBottom: '10px',
     fontSize: '16px',
-    borderRadius: '4px',
-    border: '1px solid #ccc',
   },
   button: {
     padding: '10px 20px',
@@ -90,6 +225,7 @@ const styles : { [key: string]: React.CSSProperties} = {
     border: 'none',
     borderRadius: '4px',
     cursor: 'pointer',
+    margin: '10px',
   },
 };
 
