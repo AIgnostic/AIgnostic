cb0290f42f86cdd9deb9007dade258c31d646593
Vedagiri, Prabhas <prabhas.vedagiri@outlook.com>
2025-01-23 09:21:42 +0000

    (Fix): Update test run to start and terminate server using should_exit bool [pv622]

--- aignostic/tests/dataset_api/unit/test_dataset_api.py
+++ aignostic/tests/dataset_api/unit/test_dataset_api.py
@@ -31,15 +31,20 @@ def test_client_returns_invalid_data_correctly():
 # Server tests
 @pytest.fixture(scope="module")
 def start_mock_server():
-    def run_mock_server():
-        import uvicorn
-        uvicorn.run(client_mock, host="127.0.0.1", port=5000)
+    import uvicorn
+    config = uvicorn.Config(app=client_mock, host="127.0.0.1", port=5000)
+    server = uvicorn.Server(config)
 
+    def run_mock_server():
+        nonlocal server
+        server.run()
+    
     thread = Thread(target=run_mock_server)
-    thread.daemon = True
     thread.start()
     yield
+    server.should_exit = True
     thread.join()
+
 
 
 def test_server_validates_client_dataset_correctly_given_valid_url(start_mock_server):
