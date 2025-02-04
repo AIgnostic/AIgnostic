a7a014e7e935043ab0b4bce71f0a140fa8eb07b3
AaronTomThomas <at2222@ic.ac.uk>
2025-02-04 11:47:03 +0000

    (Chore): Test consumer and producer logic [at2222]

--- aignostic/test_producer.py
+++ aignostic/test_producer.py
@@ -1,17 +1,18 @@
 import pika
 import time
+import os
 
 def connect_to_rabbitmq():
+    host = os.environ.get("RABBITMQ_HOST", "localhost")
     for i in range(10):  # Retry up to 10 times
         try:
             connection = pika.BlockingConnection(
-                pika.ConnectionParameters(host='rabbitmq')
+                pika.ConnectionParameters(host=host, heartbeat=600)
             )
             return connection
         except pika.exceptions.AMQPConnectionError:
             print(f"Connection failed. Retrying {i+1}/10...")
             time.sleep(3)
-
     raise Exception("Could not connect to RabbitMQ after multiple attempts.")
 
 connection = connect_to_rabbitmq()
@@ -26,4 +27,3 @@ while True:
     message = "Hello, RabbitMQ!"
     channel.basic_publish(exchange='', routing_key='job_queue', body=message)
     print(f"[x] Sent: {message}")
-    time.sleep(2)  # Send a message every 2 seconds
