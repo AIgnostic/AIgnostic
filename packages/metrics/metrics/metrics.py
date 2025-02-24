(Test): Add tests for explainability fidelity and fix incorrect calculations [pv622]

packages/metrics/metrics/metrics.py
@@ -516,7 +516,7 @@ def explanation_sparsity_score(info: CalculateRequest) -> float:
     #  - then check proportion less than 2 sigma from the mean
     # Note unnormalised inputs may have a volatile stability scores due to varying gradients
     # leading to greater variations in mean and std
-    lime_explanation_coeffs, _ = _lime_explanation(info, regression=True)
+    lime_explanation_coeffs, _ = _lime_explanation(info, esp=True)
     mean = np.mean(lime_explanation_coeffs)
     std = np.std(lime_explanation_coeffs)
 
@@ -549,15 +549,18 @@ def explanation_fidelity_score(info: CalculateRequest) -> float:
     # regression model predicts *probability* not prediction (after updating lime explanation)
 
     # TODO: Update fidelity_fn implementation after discussion with supervisor
+    # Used L-1 Norm for now -> using L2 norm may mean we have to divide by sqrt(N) instead of N
     def fidelity_fn(x, y) -> float:
-        return np.linalg.norm(x - y)
+        return np.linalg.norm(x - y, 1)
 
-    return 1 - np.mean(
-        fidelity_fn(
-            info.confidence_scores,
-            reg_model.predict(info.input_features)
-        )
-    ).item()
+    print(info.confidence_scores)
+    ps = reg_model.predict(info.input_features).reshape(-1, 1)
+    print(ps)
+    print(fidelity_fn(info.confidence_scores, ps))
+    print(f"len info.confidence_scores: {len(info.confidence_scores)}")
+    subtracted = fidelity_fn(info.confidence_scores, ps) / len(info.confidence_scores)
+    print(f"subtracted: {subtracted}")
+    return 1 - subtracted
 
 
 """

packages/metrics/metrics/utils.py
@@ -64,8 +64,8 @@ def _fgsm_attack(x: np.array, gradient: np.array, epsilon: float) -> np.array:
     return x_adv
 
 
-#TODO: Refactor regression flag (modified to make ESP pass)
-def _lime_explanation(info: CalculateRequest, kernel_width: float = 0.75, regression=False) -> np.ndarray:
+# TODO: Refactor ESP flag to cleaner alternative (modified to make ESP pass)
+def _lime_explanation(info: CalculateRequest, kernel_width: float = 0.75, esp=False) -> np.ndarray:
     """
     Compute LIME explanation for a black-box model.
 
@@ -73,6 +73,8 @@ def _lime_explanation(info: CalculateRequest, kernel_width: float = 0.75, regres
         info: information required to compute the explanation including info.input_features,
             confidence_scores, model_url and model_api_key
         kernel_width: Width of the Gaussian kernel for weighting
+        esp: flag indicating if the metric to compute is Explainability Sparsity Score (ESP always
+            uses predictions - not probabilities) # TODO: Check if ESP really does always use predictions
 
     Returns:
         explanation: Linear surrogate model coefficients (d-dimensional array)
@@ -90,11 +92,16 @@ def _lime_explanation(info: CalculateRequest, kernel_width: float = 0.75, regres
         scale=0.1 * np.std(info.input_features, axis=0),
         size=(num_samples, d)
     )
+
+    if not info.regression_flag:
+        # probabilities cannot exceed 1 or be less than 0
+        perturbed_samples = np.clip(perturbed_samples, 0, 1)
+
     # Call model endpoint to get confidence scores
     response: ModelResponse = _query_model(perturbed_samples, info)
-
+    print(f"Model Response = {response}")
     # Compute model probabilities for perturbed samples
-    outputs = response.predictions if (info.regression_flag or regression) else response.confidence_scores
+    outputs = response.predictions if (info.regression_flag or esp) else response.confidence_scores
 
     if outputs is None:
         raise ModelQueryException(