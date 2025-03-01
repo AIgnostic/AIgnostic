2826fc020954a18acb309ffa3521e154888854d9
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2025-03-01 23:42:21 +0000

    (Test): Fix test for redis setup

--- packages/dispatcher/tests/test_dispatcher.py
+++ packages/dispatcher/tests/test_dispatcher.py
@@ -1,14 +1,16 @@
 from typing import List
+import uuid
+
+from common.models.pipeline import MetricCalculationJob, PipelineJob
 import pytest
 import asyncio
 from unittest.mock import AsyncMock, MagicMock
 from dispatcher.dispatcher import Dispatcher
-from common.models.common import Job
 from dispatcher.models import RunningJob
 
 
 @pytest.mark.asyncio
-async def test_should_load_job_to_redis():
+async def test_should_load_job_to_redis_and_dispatch():
     """Should load a job onto redis"""
     # Mock the Redis client
     mock_redis_client = AsyncMock()
@@ -19,35 +21,38 @@ async def test_should_load_job_to_redis():
     # Create a Dispatcher instance with the mocked Redis client and connection
     dispatcher = Dispatcher(mock_connection, mock_redis_client)
 
+    dispatcher.dispatch_as_required = AsyncMock()
+
     # Create a sample job
-    job = Job(
-        user_id="user123",
-        max_concurrenct_batches=5,
+    job = PipelineJob(
+        job_id=str(uuid.uuid4()),
+        max_concurrent_batches=5,
         batch_size=10,
-        total_sample_size=100,
-        metrics=["accuracy"],
-        model_type="linear",
-        data_url="https://example.com/data",
-        model_url="https://example.com/model",
-        data_api_key="data_key",
-        model_api_key="model_key",
+        batches=10,
+        metrics=MetricCalculationJob(
+            data_url="http://example.com/data",
+            model_url="http://example.com/model",
+            data_api_key="data_api_key",
+            model_api_key="model_api_key",
+            metrics=["accuracy"],
+            model_type="model_type",
+        ),
     )
 
     # Call process_new_job with the sample job
     await dispatcher.process_new_job(job)
 
     # Check if the job was added to Redis
-    # mock_redis_client.set.assert_called_once()
-    # args, kwargs = mock_redis_client.set.call_args
-    # args: List[str]
-    # running_job = RunningJob(
-    #     job_data=job,
-    #     user_id=job.user_id,
-    #     max_concurrent_batches=job.max_concurrenct_batches,
-    #     currently_running_batches=0,
-    #     completed_batches=0,
-    #     errored_batches=0,
-    #     pending_batches=
-    # )
-    # assert job.user_id in args[0]
-    # assert args[1] == running_job.model_dump_json()
+    mock_redis_client.set.assert_called_once()
+    args, kwargs = mock_redis_client.set.call_args
+    args: List[str]
+    running_job = RunningJob(
+        job_data=job,
+        currently_running_batches=0,
+        completed_batches=0,
+        errored_batches=0,
+        pending_batches=10,
+    )
+    assert str(job.job_id) in args[0]
+    assert args[1] == running_job.model_dump_json()
+    assert dispatcher.dispatch_as_required.called
