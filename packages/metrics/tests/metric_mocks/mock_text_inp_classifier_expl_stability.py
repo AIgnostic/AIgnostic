955d3c960528c9656860e3d525273fbcccdeb48b
Vedagiri, Prabhas <prabhas.vedagiri@outlook.com>
2025-03-03 19:02:42 +0000

    (Test): Add tests for ntg expl stability [pv622]

--- packages/metrics/tests/metric_mocks/mock_text_inp_classifier_expl_stability.py
+++ packages/metrics/tests/metric_mocks/mock_text_inp_classifier_expl_stability.py
@@ -12,7 +12,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-TEST_INPUT = ModelInput(
+TEST_INPUT_TEXT_CLASSIFICATION = ModelInput(
     features=[
         ["The company's shares rose 20 pc after the announcement of the new product line."],
         ["The company's bankruptcy filing caused a massive sell-off, leading to a sharp decline in stock prices."]
@@ -24,8 +24,20 @@ TEST_INPUT = ModelInput(
     group_ids=[0] * 2
 )
 
+TEST_INPUT_NEXT_TOKEN_GENERATION = ModelInput(
+    features=[
+        ["How are you doing today?"],
+        ["What is the weather like in London right now?"]
+    ],
+    labels=[
+        ['I am doing well.'],
+        ['It\'s raining - big surprise!']
+    ],
+    group_ids=[0] * 2
+)
 
-def generate_test_responses(stability: str, num_samples):
+
+def generate_test_responses_text_classification(stability: str, num_samples: int):
     """
     Generate test responses for high or low stability
 
@@ -52,11 +64,55 @@ def generate_test_responses(stability: str, num_samples):
     )
 
 
+def generate_test_responses_ntg(stability: str, num_samples: int):
+    """
+    Generate test responses for high or low stability
+
+    :param stability: str ['high' or 'low'] - the stability of the model responses
+    :param multiplier: int - the number of times to repeat the test input
+    :return: ModelResponse - the model response object
+    """
+    predictions = []
+    if stability == "high":
+        predictions = [
+            ['I am doing well.']
+        ] * num_samples
+    elif stability == "low":
+        options = [
+            'I', 'hello', 'weather', 'is', 'terrible', 'world', 'america', 'alan',
+            'turing', 'apple', 'microsoft', 'bill', 'gates', 'car', 'house', 'dog',
+            'cat', 'bird', 'plane', 'train', 'bus', 'subway', 'taxi', 'uber', 'lyft',
+            'food', 'drink', 'water', 'soda', 'juice', 'milk', 'coffee', 'tea', 'beer',
+            'germany', 'france', 'spain', 'italy', 'england', 'scotland', 'wales', 'ireland',
+            'hot', 'cold', 'warm', 'freezing', 'boiling', 'cool', 'mild', 'moderate', 'extreme',
+            'fast', 'slow', 'quick', 'rapid', 'speedy', 'leisurely', 'relaxed', 'calm',
+            'peaceful', 'quiet', 'noisy', 'loud', 'silent', 'talkative', 'chatty', 'excited'
+        ]
+        output_lengths = np.random.randint(1, 20, size=num_samples)
+        predictions = [
+            [' '.join(np.random.choice(options, size=length))]
+            for length in output_lengths
+        ]
+    return ModelResponse(
+        predictions=predictions,
+    )
+
+
 @app.post('/predict-hs', response_model=ModelResponse)
 async def predict_hs(input_data: ModelInput):
-    return generate_test_responses('high', len(input_data.features))
+    return generate_test_responses_text_classification('high', len(input_data.features))
 
 
 @app.post('/predict-ls', response_model=ModelResponse)
 async def predict_ls(input_data: ModelInput):
-    return generate_test_responses('low', len(input_data.features))
+    return generate_test_responses_text_classification('low', len(input_data.features))
+
+
+@app.post('/predict-hs-ntg', response_model=ModelResponse)
+async def predict_hs_ntg(input_data: ModelInput):
+    return generate_test_responses_ntg('high', len(input_data.features))
+
+
+@app.post('/predict-ls-ntg', response_model=ModelResponse)
+async def predict_ls_ntg(input_data: ModelInput):
+    return generate_test_responses_ntg('low', len(input_data.features))
--- packages/metrics/tests/test_text_inp_explanation_metrics.py
+++ packages/metrics/tests/test_text_inp_explanation_metrics.py
@@ -8,7 +8,10 @@ from tests.server_factory import (
 )
 from fastapi.testclient import TestClient
 from metrics.metrics import calculate_metrics
-from tests.metric_mocks.mock_text_inp_classifier_expl_stability import TEST_INPUT
+from tests.metric_mocks.mock_text_inp_classifier_expl_stability import (
+    TEST_INPUT_TEXT_CLASSIFICATION,
+    TEST_INPUT_NEXT_TOKEN_GENERATION
+)
 
 server_factory = server_factory  # To suppress lint errors
 
@@ -23,72 +26,114 @@ def apply_server_factory(server_factory):
         yield
 
 
-def test_ntg_explanation_stability(apply_server_factory):
+# def test_ntg_explanation_stability(apply_server_factory):
+#     metric_name = "expl_stability_text_input"
+
+#     response = client.post("/predict-hs", json=TEST_INPUT_TEXT_CLASSIFICATION.model_dump(mode="json"))
+#     assert response.status_code == 200, response.text
+
+#     model_resp: ModelResponse = response.json()
+#     assert model_resp["confidence_scores"], model_resp
+
+#     # Check similar predictions after perturbation have value close to 1
+#     hs_info = CalculateRequest(
+#         batch_size=10,
+#         total_sample_size=10,
+#         metrics=[metric_name],
+#         input_features=TEST_INPUT_TEXT_CLASSIFICATION.features,
+#         confidence_scores=model_resp["confidence_scores"],
+#         model_url=f"http://{HOST}:{server_configs[mock_name]['port']}/predict-hs",
+#         model_api_key="None"
+#     )
+
+#     hs_result = calculate_metrics(hs_info)
+#     assert isinstance(hs_result.metric_values[metric_name], MetricValue), hs_result
+
+#     response = client.post("/predict-ls", json=TEST_INPUT_TEXT_CLASSIFICATION.model_dump(mode="json"))
+#     assert response.status_code == 200, response.text
+
+#     model_resp: ModelResponse = response.json()
+#     assert model_resp["confidence_scores"], model_resp
+
+#     ls_info = CalculateRequest(
+#         batch_size=10,
+#         total_sample_size=10,
+#         metrics=[metric_name],
+#         input_features=TEST_INPUT_TEXT_CLASSIFICATION.features,
+#         confidence_scores=model_resp["confidence_scores"],
+#         model_url=f"http://{HOST}:{server_configs[mock_name]['port']}/predict-ls",
+#     )
+
+#     ls_result = calculate_metrics(ls_info)
+
+#     assert isinstance(ls_result.metric_values[metric_name], MetricValue), ls_result
+
+#     assert (
+#         hs_result.metric_values[metric_name].computed_value
+#         > ls_result.metric_values[metric_name].computed_value
+#     ), "Stronger examples should have higher stability scores than ambiguous datapoints"
+
+
+# def test_high_stability_score(apply_server_factory):
+#     metric_name = "expl_stability_text_input"
+
+#     response = client.post("/predict-hs", json=TEST_INPUT_TEXT_CLASSIFICATION.model_dump(mode="json"))
+
+#     model_resp: ModelResponse = response.json()
+#     assert model_resp["confidence_scores"], model_resp
+
+#     hs_info = CalculateRequest(
+#         batch_size=10,
+#         total_sample_size=10,
+#         metrics=[metric_name],
+#         input_features=TEST_INPUT_TEXT_CLASSIFICATION.features,
+#         confidence_scores=model_resp["confidence_scores"],
+#         model_url=f"http://{HOST}:{server_configs[mock_name]['port']}/predict-hs",
+#     )
+
+#     hs_result = calculate_metrics(hs_info)
+#     assert isinstance(hs_result.metric_values[metric_name], MetricValue), hs_result
+
+#     assert hs_result.metric_values[metric_name].computed_value == 1.0, hs_result
+
+
+def test_high_stability_ntg(apply_server_factory):
     metric_name = "expl_stability_text_input"
 
-    response = client.post("/predict-hs", json=TEST_INPUT.model_dump(mode="json"))
-    assert response.status_code == 200, response.text
-
-    model_resp: ModelResponse = response.json()
-    assert model_resp["confidence_scores"], model_resp
-
-    # Check similar predictions after perturbation have value close to 1
     hs_info = CalculateRequest(
         batch_size=10,
         total_sample_size=10,
         metrics=[metric_name],
-        input_features=TEST_INPUT.features,
-        confidence_scores=model_resp["confidence_scores"],
-        model_url=f"http://{HOST}:{server_configs[mock_name]['port']}/predict-hs",
-        model_api_key="None"
-    )
-
-    hs_result = calculate_metrics(hs_info)
-    assert isinstance(hs_result.metric_values[metric_name], MetricValue), hs_result
-
-    response = client.post("/predict-ls", json=TEST_INPUT.model_dump(mode="json"))
-    assert response.status_code == 200, response.text
-
-    model_resp: ModelResponse = response.json()
-    assert model_resp["confidence_scores"], model_resp
-
-    ls_info = CalculateRequest(
-        batch_size=10,
-        total_sample_size=10,
-        metrics=[metric_name],
-        input_features=TEST_INPUT.features,
-        confidence_scores=model_resp["confidence_scores"],
-        model_url=f"http://{HOST}:{server_configs[mock_name]['port']}/predict-ls",
-    )
-
-    ls_result = calculate_metrics(ls_info)
-
-    assert isinstance(ls_result.metric_values[metric_name], MetricValue), ls_result
-
-    assert (
-        hs_result.metric_values[metric_name].computed_value
-        > ls_result.metric_values[metric_name].computed_value
-    ), "Stronger examples should have higher stability scores than ambiguous datapoints"
-
-
-def test_high_stability_score(apply_server_factory):
-    metric_name = "expl_stability_text_input"
-
-    response = client.post("/predict-hs", json=TEST_INPUT.model_dump(mode="json"))
-
-    model_resp: ModelResponse = response.json()
-    assert model_resp["confidence_scores"], model_resp
-
-    hs_info = CalculateRequest(
-        batch_size=10,
-        total_sample_size=10,
-        metrics=[metric_name],
-        input_features=TEST_INPUT.features,
-        confidence_scores=model_resp["confidence_scores"],
-        model_url=f"http://{HOST}:{server_configs[mock_name]['port']}/predict-hs",
+        task_name="next_token_generation",
+        input_features=TEST_INPUT_TEXT_CLASSIFICATION.features,
+        model_url=f"http://{HOST}:{server_configs[mock_name]['port']}/predict-hs-ntg",
+        model_api_key="None",
+        # Update metrics library to only require confidence score based on metric, task_name combination
+        confidence_scores=[[1]] * 10
     )
 
     hs_result = calculate_metrics(hs_info)
     assert isinstance(hs_result.metric_values[metric_name], MetricValue), hs_result
 
     assert hs_result.metric_values[metric_name].computed_value == 1.0, hs_result
+
+
+def test_low_stability_ntg(apply_server_factory):
+    metric_name = "expl_stability_text_input"
+
+    ls_info = CalculateRequest(
+        batch_size=10,
+        total_sample_size=10,
+        metrics=[metric_name],
+        task_name="next_token_generation",
+        input_features=TEST_INPUT_TEXT_CLASSIFICATION.features,
+        model_url=f"http://{HOST}:{server_configs[mock_name]['port']}/predict-ls-ntg",
+        model_api_key="None",
+        # Update metrics library to only require confidence score based on metric, task_name combination
+        confidence_scores=[[1]] * 10
+    )
+
+    ls_result = calculate_metrics(ls_info)
+    assert isinstance(ls_result.metric_values[metric_name], MetricValue), ls_result
+
+    assert ls_result.metric_values[metric_name].computed_value < 0.3, ls_result
