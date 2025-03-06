e706474bd5346c740c4539041bee5bfbd5fc0fac
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2025-03-06 22:30:57 +0000

    (Feat): Add new publisher long lived class

---
+++ packages/common/common/rabbitmq/publisher.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=C0111,C0103,R0205
+
+import os
+import threading
+from time import sleep
+from pika import ConnectionParameters, BlockingConnection, PlainCredentials
+from pika.adapters.blocking_connection import BlockingChannel
+
+
+RABBITMQ_USER = os.getenv("RABBITMQ_USER", "guest")
+RABBITMQ_PASS = os.getenv("RABBITMQ_PASS", "guest")
+
+
+class Publisher(threading.Thread):
+    """
+    Long lived publisher that publishes messages to a RabbitMQ queue from https://github.com/pika/pika/blob/main/examples/long_running_publisher.py.
+    Uses its own thread so it can respond to rabbitmq heartbeats and other events while the main thread is doing other things.
+
+    Usage:
+    publisher = Publisher(queue="my_queue")
+    publisher.start()
+    try:
+        publisher.publish("Hello, World!")
+        // publisher.stop()
+    finally:
+        publisher.join()
+    """
+
+    daemon = True
+    is_running = True
+    name = "Publisher"
+
+    queue: str
+    connection: BlockingConnection
+    channel: BlockingChannel
+
+    def __init__(
+        self,
+        queue: str,
+        name: str = "Publisher",
+        host: str = os.getenv("RABBITMQ_HOST", "rabbitmq"),
+        credentials: PlainCredentials = PlainCredentials(RABBITMQ_USER, RABBITMQ_PASS),
+        *args,
+        **kwargs,
+    ):
+        """Create a publisher for a channel that lives on its own thread and publishes messages to a RabbitMQ queue
+
+        Args:
+            queue (str): Queue name to publish to (will be declared durable if it doesn't exist)
+            name (str, optional): Name of this publisher. Defaults to "Publisher".
+            host (str, optional): Hostname to connect to. Defaults to os.getenv("RABBITMQ_HOST", "rabbitmq").
+            credentials (PlainCredentials, optional): Creds to connect with. Defaults to PlainCredentials(RABBITMQ_USER, RABBITMQ_PASS).
+        """
+        super().__init__(*args, **kwargs)
+        self.daemon = True
+        self.is_running = True
+        self.name = name
+        self.queue = queue
+
+        parameters = ConnectionParameters(
+            host=host, heartbeat=600, credentials=credentials
+        )
+        self.connection = BlockingConnection(parameters)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.queue, durable=True)
+
+    def on_tick(self):
+        self.connection.process_data_events(time_limit=1)
+
+    def run(self):
+        while self.is_running:
+            self.on_tick()
+
+    def _publish(self, message):
+        self.channel.basic_publish("", self.queue, body=message.encode())
+
+    def publish(self, message):
+        self.connection.add_callback_threadsafe(lambda: self._publish(message))
+
+    def stop(self):
+        print("Stopping...")
+        self.is_running = False
+        # Wait until all the data events have been processed
+        self.connection.process_data_events(time_limit=1)
+        if self.connection.is_open:
+            self.connection.close()
+        print("Stopped")
+
+
+if __name__ == "__main__":
+    publisher = Publisher()
+    publisher.start()
+    try:
+        for i in range(9999):
+            msg = f"Message {i}"
+            print(f"Publishing: {msg!r}")
+            publisher.publish(msg)
+            sleep(1)
+    except KeyboardInterrupt:
+        publisher.stop()
+    finally:
+        publisher.join()
---
+++ packages/common/tests/test_publisher.py
@@ -0,0 +1,52 @@
+import pytest
+from unittest.mock import patch, MagicMock
+from common.rabbitmq.publisher import Publisher
+
+
+@pytest.fixture
+def publisher():
+    with patch("common.rabbitmq.publisher.BlockingConnection"), patch(
+        "common.rabbitmq.publisher.BlockingChannel"
+    ):
+        return Publisher(queue="test_queue")
+
+
+def test_publisher_initialization(publisher):
+    assert publisher.queue == "test_queue"
+    assert publisher.name == "Publisher"
+    assert publisher.is_running is True
+
+
+def test_publisher_publish(publisher):
+    with patch.object(publisher, "channel") as mock_channel:
+        publisher._publish("test message")
+        mock_channel.basic_publish.assert_called_once_with(
+            "", "test_queue", body=b"test message"
+        )
+        # And check we add _publish to the connection
+        publisher.publish("test message")
+        publisher.connection.add_callback_threadsafe.assert_called_once()
+
+
+
+def test_publisher_stop(publisher):
+    with patch.object(publisher.connection, "close") as mock_close:
+        publisher.stop()
+        assert publisher.is_running is False
+        mock_close.assert_called_once()
+
+
+def test_publisher_tick(publisher):
+    with patch.object(publisher.connection, "process_data_events") as mock_process:
+        publisher.on_tick()
+        mock_process.assert_called()
+
+
+def test_publisher_run(publisher):
+    # Make on_tick immediately throw an exception, expect that to happen and check it was called
+    with patch(
+        "common.rabbitmq.publisher.Publisher.on_tick", side_effect=Exception
+    ) as mock_on_tick:
+        with pytest.raises(Exception):
+            publisher.run()
+        mock_on_tick.assert_called()
